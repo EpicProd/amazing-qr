462b7ed9516cc9c25c60532e9c8c5bb138303ca7
rf0x3d <yarekfrolov@gmail.com>
2022-10-30 23:12:28 +0300

    make qr transparent
    Signed-off-by: rf0x3d <yarekfrolov@gmail.com>

--- amzqr/mylibs/draw.py
+++ amzqr/mylibs/draw.py
@@ -6,7 +6,7 @@ import os
 def draw_qrcode(abspath, qrmatrix):
     unit_len = 3
     x = y = 4*unit_len
-    pic = Image.new('1', [(len(qrmatrix)+8)*unit_len]*2, 'white')
+    pic = Image.new('RGBA', [(len(qrmatrix)+8)*unit_len]*2, (255, 255, 255, 0))
     
     for line in qrmatrix:
         for module in line:
@@ -18,8 +18,9 @@ def draw_qrcode(abspath, qrmatrix):
     saving = os.path.join(abspath, 'qrcode.png')
     pic.save(saving)
     return saving
-    
+
+
 def draw_a_black_unit(p, x, y, ul):
     for i in range(ul):
         for j in range(ul):
-            p.putpixel((x+i, y+j), 0)
+            p.putpixel((x+i, y+j), (255, 255, 255, 255))
